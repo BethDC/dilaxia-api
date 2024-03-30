7999e16665fb2955d620919b17a8b03b4e42df6e
aintDatCap <federico.caprini@protonmail.com>
2024-03-30 16:50:06 +0100

    Performance improvements

--- src/main/java/it/avbo/dilaxia/api/database/DBWrapper.java
+++ src/main/java/it/avbo/dilaxia/api/database/DBWrapper.java
@@ -19,7 +19,7 @@ public class DBWrapper {
             dataSource.setPassword(null);
 
             dbConnection = dataSource.getConnection();
-            //setupDatabase();
+            //DBWrapper.setupDatabase();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -30,6 +30,7 @@ public class DBWrapper {
             SELECT *
             FROM users
             WHERE users.username = ?
+            LIMIT 1
         """)) {
             statement.setString(1, username);
             ResultSet resultSet = statement.executeQuery();
@@ -50,6 +51,33 @@ public class DBWrapper {
             return Optional.empty();
         }
     }
+    public static Optional<User> getUserByEmail(@NonNull String email) {
+        try(PreparedStatement statement = dbConnection.prepareStatement("""
+            SELECT *
+            FROM users
+            WHERE users.email = ?
+            LIMIT 1
+        """)) {
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()) {
+                return Optional.of(
+                        new User(
+                                resultSet.getString("username"),
+                                resultSet.getString("email"),
+                                UserRole.fromValue(resultSet.getInt("role")),
+                                resultSet.getBytes("password_hash"),
+                                resultSet.getBytes("salt")
+                        )
+                );
+            }
+            return Optional.empty();
+
+        } catch (SQLException e) {
+            return Optional.empty();
+        }
+    }
+
     public static boolean addUser(@NonNull User user) {
         try(PreparedStatement statement = dbConnection.prepareStatement("""
             INSERT INTO users(username, email, password_hash, salt, role)
@@ -69,18 +97,19 @@ public class DBWrapper {
 
     public static void setupDatabase() {
         String dbInitialization = """
-            USE dilaxia;
-            DROP TABLE IF EXISTS users;
-            CREATE TABLE users (
-                username VARCHAR(30) PRIMARY KEY,
-                email VARCHAR(50) NOT NULL UNIQUE,
-                password_hash VARBINARY(256) NOT NULL,
-                salt VARBINARY(64) NOT NULL,
-                user_role TINYINT NOT NULL
-            );
+        USE dilaxia;
+            
+        CREATE OR REPLACE TABLE users (
+            username VARCHAR(30) PRIMARY KEY,
+            email VARCHAR(50) NOT NULL UNIQUE,
+            password_hash VARBINARY(256) NOT NULL,
+            salt VARBINARY(64) NOT NULL,
+            role TINYINT NOT NULL
+        );
         """;
+
         try(Statement statement = dbConnection.createStatement()) {
-            statement.executeUpdate(dbInitialization);
+            statement.execute(dbInitialization);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
