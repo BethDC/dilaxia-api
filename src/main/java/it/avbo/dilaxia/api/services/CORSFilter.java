12e14fb675e4c5201586686cccc16d2aabfc2197
aintDatCap <federico.caprini@protonmail.com>
2024-04-07 09:47:57 +0200

    Added CORS filter

---
+++ src/main/java/it/avbo/dilaxia/api/services/CORSFilter.java
@@ -0,0 +1,21 @@
+package it.avbo.dilaxia.api.services;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        String reqHeader = requestContext.getHeaderString("Access-Control-Request-Headers");
+        if (reqHeader != null && !reqHeader.isEmpty()) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", reqHeader);
+        }
+    }
+}
